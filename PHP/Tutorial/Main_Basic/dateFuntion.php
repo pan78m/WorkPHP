5bec59ce738c701e3f5eb74414cf08665a52a724
Pankaj Mahanto <aryanpankaj78@gmail.com>
2023-03-19 22:16:08 +0600

    d

---
+++ PHP/Tutorial/Main_Basic/dateFuntion.php
@@ -0,0 +1,14 @@
+<?php 
+
+ echo "Welcome to the world of dates!!<br>";
+
+ $d=date("dS F Y");
+ echo "Today is the day of the week $d<br>";
+
+ $d=date("N j M x");
+ echo "Today is the day of the week $d<br>";
+
+ $d=date("D L n X");
+ echo "Today is the day of the week $d<br>";
+
+?>
